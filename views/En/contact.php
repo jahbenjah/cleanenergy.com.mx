340206254e2b6c4c1fd0a31cf7ee6385de4ff116
maurialcala <38312299+maurialcala@users.noreply.github.com>
2020-07-14 16:10:33 -0700

    send

--- views/En/contact.php
+++ views/En/contact.php
@@ -170,7 +170,7 @@
                 <div class="error-message"></div>
                 <div class="sent-message">Your message has been sent. Thank you!</div>
               </div>
-              <div class="text-center"><button type="submit">Enviar Mensaje</button></div>
+              <div class="text-center"><button type="submit">Send Message</button></div>
             </form>
           </div>
         </div>
