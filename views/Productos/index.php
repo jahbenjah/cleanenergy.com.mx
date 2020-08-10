5679e0256642aa9949154296dcac96188a4913fb
maurialcala <38312299+maurialcala@users.noreply.github.com>
2020-08-10 15:16:35 -0700

    Test

--- views/Productos/index.php
+++ views/Productos/index.php
@@ -156,7 +156,7 @@
       <div class="container">
 
         <div class="d-flex justify-content-between align-items-center">
-          <h2>Productos</h2>
+          <h2>Productos Testing</h2>
           <ol>
             <li><a href="index.html">Inicio</a></li>
             <li>Productos</li>
