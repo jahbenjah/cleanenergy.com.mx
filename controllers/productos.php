1d729b5c7fa4ca9aeb5f7abc702ca2bc84e0f35f
maurialcala <38312299+maurialcala@users.noreply.github.com>
2020-08-10 13:45:29 -0700

    productos

--- controllers/productos.php
+++ controllers/productos.php
@@ -8,18 +8,7 @@ class Productos extends Controller
 
 	function index()
 	{
-		$this->view->productos = $this->model->productos('Paneles Solares');
 		$this->view->render($this, 'index');
-	}
-	function Categoria($cat)
-	{
-		$this->view->productos = $this->model->interconexion($cat);
-		$this->view->render($this, 'index');
-	}
-	function producto($id)
-	{
-		$this->view->productDetails = $this->model->productDetails($id);
-		$this->view->render($this, 'producto');
 	}
 }
 ?>
