5283115da103be43edbfe7cb4eefe5791494e4d2
maurialcala <38312299+maurialcala@users.noreply.github.com>
2020-08-11 13:16:36 -0700

    productos

--- models/Productos_model.php
+++ models/Productos_model.php
@@ -9,7 +9,7 @@
 			return $this->db->select($sql);
 		}
 		function productDetails($id){
-			$sql="SELECT * from `interconexion` p 
+			$sql="SELECT * from `productos` p 
 				WHERE p.id=:id";
 			return $this->db->select($sql, array('id' =>$id));
 		}
