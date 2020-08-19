3b3ef3315f4c9fe214ec10693cc09c455828df7f
Benjamin Camacho <jahbenjah@hotmail.com>
2020-08-18 23:12:40 -0500

    Prod

--- views/Dashboard/index.php
+++ views/Dashboard/index.php
@@ -35,10 +35,7 @@
       <h5 class="my-0 mr-md-auto font-weight-normal">Clean Energy</h5>
       <nav class="my-2 my-md-0 mr-md-3">
         <a class="p-2 text-dark" href="<?=URL?>dashboard/user">Usuarios</a>
-        <span class="border-bottom"><a class="p-2  text-info" href="<?=URL?>dashboard/">Interconexion</a></span>
-        <a class="p-2  text-dark" href="<?=URL?>dashboard/autonomos">Autonomos</a> 
-        <a class="p-2  text-dark" href="<?=URL?>dashboard/luminarias">Luminarias</a> 
-
+        <span class="border-bottom"><a class="p-2  text-info" href="<?=URL?>dashboard/">Productos</a></span>
       </nav>
       <a class="btn btn-outline-primary" href="<?=URL?>dashboard/destroySession">Salir</a>
     </div>
