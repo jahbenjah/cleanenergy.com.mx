6345a26a465199ace5afead116c8282720c8f1eb
maurialcala <38312299+maurialcala@users.noreply.github.com>
2020-08-18 21:34:37 -0700

    banner

--- views/Index/index.php
+++ views/Index/index.php
@@ -171,7 +171,7 @@
         </div>
 
         <!-- Slide 2 -->
-        <div class="carousel-item" style="background-image: url(/public/img/Solar-Panels-US.jpg);">
+        <div class="carousel-item" style="background-image: url(/public/img/bannerpagina.png);">
           <div class="carousel-container">
             <div class="carousel-content animate__animated animate__fadeInUp">
               <h2 class="text-center"><span>Somos El Centro de Distribución Solar más completo de México</span></h2>
@@ -182,7 +182,7 @@
         </div>
 
         <!-- Slide 3 -->
-        <div class="carousel-item" style="background-image: url(/public/img/Solar-2.jpg);">
+        <div class="carousel-item" style="background-image: url(/public/img/banner4.jpg);">
           <div class="carousel-container">
             <div class="carousel-content animate__animated animate__fadeInUp">
               <h2 class="text-center"><span>Somos El Centro de Distribución Solar más completo de México</span></h2>
