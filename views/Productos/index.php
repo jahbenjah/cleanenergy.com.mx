3c4abbfcfa63e8b5f3f33c22ab667f5b7258ad83
maurialcala <38312299+maurialcala@users.noreply.github.com>
2020-09-04 15:04:53 -0700

    f

--- views/Productos/index.php
+++ views/Productos/index.php
@@ -190,14 +190,13 @@ $productos = $this->productos;
           </div>
         </div>
 
-        <div class="container pt-5">
+        <div class="container">
           <div class="row justify-content-center animatedParent animateOnce">
             <?php
               for ($i = 0; $i < count($productos); $i++) {
            ?>
-              <div class="col-lg-3 animated slowest fadeInLeft delay-500">
-            <div class="col-lg-4 col-md-6 portfolio-item filter-<?= html_entity_decode($productos[$i]['categoria']) ?>">
-             <div style="max-width:200px">    
+            <div class="col-lg-3 animated slowest fadeInLeft delay-500"><?= html_entity_decode($productos[$i]['categoria']) ?>">
+             <div style="max-width:300px">    
              <div class="product-cuadro img-rounded zoom">  
                <img loading="lazy"
                src="<?= URL ?>public/imagenespdfs/<?= html_entity_decode($productos[$i]['Imagen']) ?>" 
