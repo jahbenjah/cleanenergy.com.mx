44826d1fb909875f4b444bc969371f2ed132a6f3
maurialcala <38312299+maurialcala@users.noreply.github.com>
2020-09-04 23:09:02 -0700

    test

--- views/Productos/index.php
+++ views/Productos/index.php
@@ -197,16 +197,16 @@ $productos = $this->productos;
            <div class="col-lg-3 animated slowest fadeInLeft delay-500">
              
             <div style="max-width:200px">
-            
-                <?= html_entity_decode($productos[$i]['categoria']) ?>">
-   
+
                <img loading="lazy"
                src="<?= URL ?>public/imagenespdfs/<?= html_entity_decode($productos[$i]['Imagen']) ?>" 
                class="img-fluid" alt="<?= html_entity_decode($productos[$i]['Nombre']) ?>">
                <div class="row justify-content-center pt-4 pb-3">
                   <p class="text-uppercase fontGeneral">
+                  <center>
                 <?= html_entity_decode($productos[$i]['Nombre']) ?></p>
                 <p><?= html_entity_decode($productos[$i]['Marca']) ?></p>
+                </center>  
                 <a href="<?= URL ?>public/imagenespdfs/<?= html_entity_decode($productos[$i]['Imagen']) ?>" 
                 class="details-link" 
                 title="<?= html_entity_decode($productos[$i]['Nombre']) ?>"></a>
